322df8110abaecdcecf02bf670cba82af61738d9
Ravindra Devaliya <rnd@taglineinfotech.com>
2023-03-14 18:37:05 +0530

    Implement api for display author with his own book list

--- app/serializers.py
+++ app/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import SerializerMethodField
 from app.models import Post
 from app.models import Author
 from app.models import Book
@@ -11,12 +12,24 @@ class PostSerializers(ModelSerializer):
 
 
 class AuthorSerializers(ModelSerializer):
+    books = SerializerMethodField()
+
     class Meta:
         model = Author
         fields = "__all__"
 
+    def get_books(self, instance):
+        books = Book.objects.filter(author_id=instance.id)
+        book_serializer = BookSerializers(books, many=True)
+        return book_serializer.data
+
 
 class BookSerializers(ModelSerializer):
+    author = SerializerMethodField()
+
     class Meta:
         model = Book
         fields = "__all__"
+
+    def get_author(self, instance):
+        return instance.author.name
