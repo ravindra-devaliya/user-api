3cf5cfb4f3bfe708f7e5494288d2b04d5af85f2b
Ravindra Devaliya <rnd@taglineinfotech.com>
2023-03-15 11:42:46 +0530

    Add models for custom users and user manager

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from users.models import User
+from django.contrib.auth.models import Group
 
-# Register your models here.
+admin.site.register(User)
+admin.site.unregister(Group)
